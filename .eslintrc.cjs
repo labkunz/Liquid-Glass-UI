779fc8bbd64a405f2443bdd205cdc23b1bdfdd95
Kent Chen <bboykunz@gmail.com>
2026-02-28 23:59:23 +0800

    feat(eslint): 加入 ESLint 的格式校正設定

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    // 允許單字元件名（例如 Button, Input）
+    'vue/multi-word-component-names': 'off',
+
+    // any 使用警告而非錯誤
+    '@typescript-eslint/no-explicit-any': 'warn',
+
+    // 允許未使用的變數如果以底線開頭
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
+  },
+}
